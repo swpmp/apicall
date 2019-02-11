844dde7628c16f7de0fb47262719675df3c41369
swpmp <dmytro@pumapay.io>
2019-02-11 10:32:04 +0200

    created repo

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { interval, Observable } from 'rxjs';
+import { concatMap, map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements OnInit {
+  polledBitcoin$: Observable<number>;
+  value: string;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.callBtc(); 
+  }
+
+  public callBtc() {
+    const bitcoin$ = this.http.get('https://blockchain.info/ticker');
+    return new Promise(resolve => {
+      this.polledBitcoin$ = interval(10000).pipe(
+        concatMap(_ => bitcoin$),
+        map((response: {EUR: {last: number}}) => response.EUR.last),
+      );
+    })
+  }
+}
